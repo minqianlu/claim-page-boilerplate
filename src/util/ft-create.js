6352b2f6905f954ee9e8563b1c8c1e3285645ca8
BenKurrek <bkurrek@uwaterloo.ca>
2023-07-14 13:48:19 -0400

    added account exists checker

--- src/util/ft-create.js
+++ src/util/ft-create.js
@@ -1,4 +1,4 @@
-import { claim, generateKeys, initKeypom } from '@keypom/core';
+import { accountExists, claim, generateKeys, initKeypom } from '@keypom/core';
 
 async function createAccountAndClaim(privKey, accountId, pinCode, _network) {
     const network = _network ? _network : 'testnet';
@@ -11,6 +11,7 @@ async function createAccountAndClaim(privKey, accountId, pinCode, _network) {
     await initKeypom({
         network
     });
+
     console.log('newAccountId: ', newAccountId)
     console.log('refractionKey: ', refractionKey)
 
@@ -20,11 +21,23 @@ async function createAccountAndClaim(privKey, accountId, pinCode, _network) {
     })
     console.log('secretKeys: ', secretKeys)
 
-    await claim({
-        secretKey: privKey,
-        newAccountId,
-        newPublicKey: publicKeys[0],
-    })
+    const doesAccountExist = await accountExists(newAccountId);
+    console.log('doesAccountExist: ', doesAccountExist)
+    if (doesAccountExist) {
+        console.log(doesAccountExist)
+        return {
+            error: 'Account exists',
+            account: null,
+        }
+    } else {
+        console.log('claiming account')
+        await claim({
+            secretKey: privKey,
+            newAccountId,
+            newPublicKey: publicKeys[0],
+        })
+    }
+
     
     // Generate the auto import link for the new account
     const walletAutoImportLink = `https://wallet.${network}.near.org/auto-import-secret-key#${newAccountId}/${secretKeys[0]}`;
